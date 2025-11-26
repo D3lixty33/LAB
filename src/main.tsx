c218a1d7655db6adf4474179e1c7c6c42dcff398
Alessandro Ferraro <Ferraro@SIRAC.LOCAL>
2025-11-26 12:07:51 +0100

    Added the analytics vercel tool

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/next";
 import "./index.css";
 import App from "./App.tsx";
 import ScrollWrapper from "./components/scroll-wrapper";
 
 createRoot(document.getElementById("root")!).render(
+  
     <ScrollWrapper>
       <App />
+      <Analytics></Analytics>
     </ScrollWrapper>
+
 );
