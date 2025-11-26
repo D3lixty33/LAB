73839188e7dfcba50e01a29c7f759bd170d1ea8c
Alessandro Ferraro <Ferraro@SIRAC.LOCAL>
2025-11-26 12:10:29 +0100

    Adjusting analytics tool usage

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.tsx";
 import ScrollWrapper from "./components/scroll-wrapper";
 
 createRoot(document.getElementById("root")!).render(
-  
-    <ScrollWrapper>
-      <App />
-      <Analytics></Analytics>
-    </ScrollWrapper>
-
+  <ScrollWrapper>
+    <App />
+    <Analytics />
+  </ScrollWrapper>
 );
